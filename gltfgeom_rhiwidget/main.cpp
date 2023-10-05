1a6e2ba290998a974aada48444b12ee9801600c6
Laszlo Agocs <laszlo.agocs@qt.io>
2023-10-05 12:59:44 +0200

    Add a camera and WASD movement to the widget app

--- gltfgeom_rhiwidget/main.cpp
+++ gltfgeom_rhiwidget/main.cpp
@@ -5,6 +5,8 @@
 #include <QCommandLineParser>
 #include <QElapsedTimer>
 #include <QRhiWidget>
+#include <QKeyEvent>
+#include <QQuaternion>
 #include <QFile>
 #include <QFileInfo>
 #include <rhi/qrhi.h>
@@ -172,6 +174,238 @@ bool Gltf::load(const QString &filename)
     return true;
 }
 
+struct Camera
+{
+    QVector3D position;
+    QVector3D scale = { 1, 1, 1 };
+    QVector3D pivot;
+    QVector3D eulerRotation;
+
+    void updateProjection(const QMatrix4x4 &clipSpaceCorrMatrix, float fovDegrees, float aspectRatio,
+                          float clipNear, float clipFar);
+    void updateWorldTransform();
+    void updateViewProjection();
+    void updateRotation();
+    QVector3D mapDirection(const QVector3D &localDirection) const;
+    QVector3D forward() const { return mapDirection(QVector3D(0, 0, -1)).normalized(); }
+    QVector3D up() const { return mapDirection(QVector3D(0, 1, 0)).normalized(); }
+    QVector3D right() const { return mapDirection(QVector3D(1, 0, 0)).normalized(); }
+
+    QMatrix4x4 projection;
+    QMatrix4x4 worldTransform;
+    QMatrix4x4 viewProjection;
+    QQuaternion rotation;
+};
+
+static inline QVector3D transformMat33(const QMatrix3x3 &m, const QVector3D &v)
+{
+    const QVector3D c0 = QVector3D(m(0, 0), m(1, 0), m(2, 0));
+    const QVector3D c1 = QVector3D(m(0, 1), m(1, 1), m(2, 1));
+    const QVector3D c2 = QVector3D(m(0, 2), m(1, 2), m(2, 2));
+    return c0 * v.x() + c1 * v.y() + c2 * v.z();
+}
+
+QVector3D Camera::mapDirection(const QVector3D &localDirection) const
+{
+    return transformMat33(worldTransform.normalMatrix(), localDirection);
+}
+
+void Camera::updateProjection(const QMatrix4x4 &clipSpaceCorrMatrix, float fovDegrees, float aspectRatio,
+                              float clipNear, float clipFar)
+{
+    projection = clipSpaceCorrMatrix;
+    projection.perspective(fovDegrees, aspectRatio, clipNear, clipFar);
+}
+
+void Camera::updateWorldTransform()
+{
+    worldTransform.setToIdentity();
+
+    const QVector3D offset = (-pivot * scale);
+
+    worldTransform(0, 0) = scale[0];
+    worldTransform(1, 1) = scale[1];
+    worldTransform(2, 2) = scale[2];
+
+    worldTransform(0, 3) = offset[0];
+    worldTransform(1, 3) = offset[1];
+    worldTransform(2, 3) = offset[2];
+
+    worldTransform = QMatrix4x4(rotation.toRotationMatrix()) * worldTransform;
+
+    worldTransform(0, 3) += position[0];
+    worldTransform(1, 3) += position[1];
+    worldTransform(2, 3) += position[2];
+}
+
+void Camera::updateViewProjection()
+{
+    QMatrix4x4 m(Qt::Uninitialized);
+    m.setColumn(0, worldTransform.column(0).normalized());
+    m.setColumn(1, worldTransform.column(1).normalized());
+    m.setColumn(2, worldTransform.column(2).normalized());
+    m.setColumn(3, worldTransform.column(3));
+    viewProjection = projection * m.inverted();
+}
+
+void Camera::updateRotation()
+{
+    rotation = QQuaternion::fromEulerAngles(eulerRotation).normalized();
+}
+
+struct WASDController
+{
+    WASDController(Camera *camera) : camera(camera) { }
+    void update();
+
+    float moveSpeed = 5.0f;
+    float xLookSpeed = 0.1f;
+    float yLookSpeed = 0.1f;
+
+    void handleKeyPress(int key);
+    void handleKeyRelease(int key);
+    void handleMousePress(const QPointF &pos);
+    void handleMouseRelease();
+    void handleMouseMove(const QPointF &pos);
+
+    Camera *camera;
+    struct {
+        bool moveForward, moveBack, moveLeft, moveRight, moveUp, moveDown;
+        bool moveMouse;
+        QPointF currentMousePos;
+        QPointF lastMousePos;
+        QElapsedTimer delta;
+        bool deltaValid;
+    } state = {};
+};
+
+void WASDController::update()
+{
+    float delta = 16.6f;
+    if (state.deltaValid) {
+        delta = state.delta.restart();
+    } else {
+        state.delta.start();
+        state.deltaValid = true;
+    }
+    delta = std::min(33.3f, std::max(0.0f, delta));
+
+    if (state.moveForward || state.moveBack || state.moveLeft || state.moveRight || state.moveUp || state.moveDown) {
+        camera->updateWorldTransform();
+        const float speed = moveSpeed * delta;
+        QVector3D direction;
+        if (state.moveForward || state.moveBack) {
+            if (state.moveForward)
+                direction = camera->forward();
+            else
+                direction = -camera->forward();
+            const QVector3D velocity(direction.x() * speed, direction.y() * speed, direction.z() * speed);
+            camera->position += velocity;
+        }
+        if (state.moveRight || state.moveLeft) {
+            if (state.moveRight)
+                direction = camera->right();
+            else
+                direction = -camera->right();
+            const QVector3D velocity(direction.x() * speed, direction.y() * speed, direction.z() * speed);
+            camera->position += velocity;
+        }
+        if (state.moveUp || state.moveDown) {
+            if (state.moveUp)
+                direction = camera->up();
+            else
+                direction = -camera->up();
+            const QVector3D velocity(direction.x() * speed, direction.y() * speed, direction.z() * speed);
+            camera->position += velocity;
+        }
+    }
+
+    if (state.moveMouse) {
+        QVector3D r = camera->eulerRotation;
+        float rx = (state.lastMousePos.x() - state.currentMousePos.x()) * xLookSpeed * delta;
+        r.setY(r.y() + rx);
+
+        float ry = -1.0f * ((state.lastMousePos.y() - state.currentMousePos.y()) * -yLookSpeed * delta);
+        r.setX(r.x() + ry);
+
+        camera->eulerRotation = r;
+        camera->updateRotation();
+
+        state.lastMousePos = state.currentMousePos;
+    }
+}
+
+void WASDController::handleKeyPress(int key)
+{
+    switch (key) {
+    case Qt::Key_W:
+        state.moveForward = true;
+        state.moveBack = false;
+        break;
+    case Qt::Key_S:
+        state.moveBack = true;
+        state.moveForward = false;
+        break;
+    case Qt::Key_D:
+        state.moveRight = true;
+        state.moveLeft = false;
+        break;
+    case Qt::Key_A:
+        state.moveLeft = true;
+        state.moveRight = false;
+        break;
+    case Qt::Key_R:
+        state.moveUp = true;
+        state.moveDown = false;
+        break;
+    case Qt::Key_F:
+        state.moveDown = true;
+        state.moveUp = false;
+        break;
+    }
+}
+
+void WASDController::handleKeyRelease(int key)
+{
+    switch (key) {
+    case Qt::Key_W:
+        state.moveForward = false;
+        break;
+    case Qt::Key_S:
+        state.moveBack = false;
+        break;
+    case Qt::Key_D:
+        state.moveRight = false;
+        break;
+    case Qt::Key_A:
+        state.moveLeft = false;
+        break;
+    case Qt::Key_R:
+        state.moveUp = false;
+        break;
+    case Qt::Key_F:
+        state.moveDown = false;
+        break;
+    }
+}
+
+void WASDController::handleMousePress(const QPointF &pos)
+{
+    state.currentMousePos = pos;
+    state.lastMousePos = pos;
+    state.moveMouse = true;
+}
+
+void WASDController::handleMouseRelease()
+{
+    state.moveMouse = false;
+}
+
+void WASDController::handleMouseMove(const QPointF &pos)
+{
+    state.currentMousePos = pos;
+}
+
 static QShader getShader(const QString &name)
 {
     QFile f(name);
@@ -184,27 +418,37 @@ public:
     ExampleRhiWidget(const QString &gltfFilename, QWidget *parent = nullptr);
     void initialize(QRhiCommandBuffer *cb) override;
     void render(QRhiCommandBuffer *cb) override;
+    void keyPressEvent(QKeyEvent *event) override;
+    void keyReleaseEvent(QKeyEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
 
 private:
     Gltf m_gltf;
+    Camera m_camera;
+    WASDController m_wasd;
     QRhi *m_rhi = nullptr;
     std::unique_ptr<QRhiBuffer> m_vbuf;
     std::unique_ptr<QRhiBuffer> m_ibuf;
     std::unique_ptr<QRhiBuffer> m_ubuf;
     std::unique_ptr<QRhiShaderResourceBindings> m_srb;
     std::unique_ptr<QRhiGraphicsPipeline> m_pipeline;
-    QMatrix4x4 m_viewProjection;
     float m_rotation = 0.0f;
 };
 
 ExampleRhiWidget::ExampleRhiWidget(const QString &gltfFilename, QWidget *parent)
-    : QRhiWidget(parent)
+    : QRhiWidget(parent),
+      m_wasd(&m_camera)
 {
     QElapsedTimer timer;
     timer.start();
+
     m_gltf.load(gltfFilename);
     if (m_gltf.isValid())
         qDebug() << "glTF asset loaded in" << timer.elapsed() << "ms";
+
+    m_camera.position = QVector3D(0, -100, -600);
 }
 
 void ExampleRhiWidget::initialize(QRhiCommandBuffer *cb)
@@ -253,17 +497,19 @@ void ExampleRhiWidget::initialize(QRhiCommandBuffer *cb)
     }
 
     const QSize outputSize = colorTexture()->pixelSize();
-    m_viewProjection = m_rhi->clipSpaceCorrMatrix();
-    m_viewProjection.perspective(45.0f, outputSize.width() / (float) outputSize.height(), 0.01f, 1000.0f);
-    m_viewProjection.translate(0, -100, -600);
-    //m_viewProjection.translate(0, 0, -10);
+    m_camera.updateProjection(m_rhi->clipSpaceCorrMatrix(), 45.0f, outputSize.width() / (float) outputSize.height(), 0.01f, 10000.0f);
 }
 
 void ExampleRhiWidget::render(QRhiCommandBuffer *cb)
 {
     QRhiResourceUpdateBatch *resourceUpdates = m_rhi->nextResourceUpdateBatch();
-    m_rotation += 0.5f;
-    QMatrix4x4 modelViewProjection = m_viewProjection;
+
+    m_wasd.update();
+    m_camera.updateWorldTransform();
+    m_camera.updateViewProjection();
+
+    QMatrix4x4 modelViewProjection = m_camera.viewProjection;
+    m_rotation += 0.2f;
     modelViewProjection.rotate(m_rotation, 0, 1, 0);
     resourceUpdates->updateDynamicBuffer(m_ubuf.get(), 0, 64, modelViewProjection.constData());
 
@@ -287,6 +533,34 @@ void ExampleRhiWidget::render(QRhiCommandBuffer *cb)
     update();
 }
 
+void ExampleRhiWidget::keyPressEvent(QKeyEvent *event)
+{
+    m_wasd.handleKeyPress(event->key());
+}
+
+void ExampleRhiWidget::keyReleaseEvent(QKeyEvent *event)
+{
+    m_wasd.handleKeyRelease(event->key());
+}
+
+void ExampleRhiWidget::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton)
+        m_wasd.handleMousePress(event->position());
+}
+
+void ExampleRhiWidget::mouseReleaseEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton)
+        m_wasd.handleMouseRelease();
+}
+
+void ExampleRhiWidget::mouseMoveEvent(QMouseEvent *event)
+{
+    if (event->buttons().testFlag(Qt::LeftButton))
+        m_wasd.handleMouseMove(event->position());
+}
+
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
@@ -296,6 +570,16 @@ int main(int argc, char **argv)
     cmdLineParser.addPositionalArgument(QLatin1String("file"),
                                         QObject::tr("glTF file (.glb or .gltf)"),
                                         QObject::tr("file"));
+    QCommandLineOption glOption({ "g", "opengl" }, QLatin1String("OpenGL"));
+    cmdLineParser.addOption(glOption);
+    QCommandLineOption vkOption({ "v", "vulkan" }, QLatin1String("Vulkan"));
+    cmdLineParser.addOption(vkOption);
+    QCommandLineOption d3d11Option({ "d", "d3d11" }, QLatin1String("Direct3D 11"));
+    cmdLineParser.addOption(d3d11Option);
+    QCommandLineOption d3d12Option({ "D", "d3d12" }, QLatin1String("Direct3D 12"));
+    cmdLineParser.addOption(d3d12Option);
+    QCommandLineOption mtlOption({ "m", "metal" }, QLatin1String("Metal"));
+    cmdLineParser.addOption(mtlOption);
 
     cmdLineParser.process(app);
 
@@ -304,9 +588,20 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    ExampleRhiWidget rhiWidget(cmdLineParser.positionalArguments().first());
-    rhiWidget.setApi(QRhiWidget::Api::D3D12);
-    rhiWidget.setDebugLayer(true);
+    const QStringList fileArgs = cmdLineParser.positionalArguments();
+    ExampleRhiWidget rhiWidget(fileArgs.first());
+
+    if (cmdLineParser.isSet(glOption))
+        rhiWidget.setApi(QRhiWidget::Api::OpenGL);
+    if (cmdLineParser.isSet(vkOption))
+        rhiWidget.setApi(QRhiWidget::Api::Vulkan);
+    if (cmdLineParser.isSet(d3d11Option))
+        rhiWidget.setApi(QRhiWidget::Api::D3D11);
+    if (cmdLineParser.isSet(d3d12Option))
+        rhiWidget.setApi(QRhiWidget::Api::D3D12);
+    if (cmdLineParser.isSet(mtlOption))
+        rhiWidget.setApi(QRhiWidget::Api::Metal);
+
     rhiWidget.resize(1280, 720);
     rhiWidget.show();
     return app.exec();
